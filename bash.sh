6db01f7970f19ecc927cf9364b8e6c6ebad8ccb6
chatofking <chat_4432@hotmail.com>
2022-11-09 10:19:43 +0700

    first commit

---
+++ bash.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sudo apt update
+sudo apt install neofetch htop -y
+curl https://raw.githubusercontent.com/Cretezy/Swap/master/swap.sh -O swap
+sh swap 4G
+sudp apt install docker.io docker-compose -y
+sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up
