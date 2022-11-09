62b79092843c94d8b9432345898211207c731fed
chatofking <chat_4432@hotmail.com>
2022-11-09 11:02:21 +0700

    bash

--- bash.sh
+++ bash.sh
@@ -3,7 +3,6 @@ sudo apt update
 sudo apt install neofetch htop -y
 curl https://raw.githubusercontent.com/Cretezy/Swap/master/swap.sh -O swap
 sh swap 4G
-sudp apt install docker.io docker-compose -y
+sudo apt install docker.io docker-compose -y
 sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up
