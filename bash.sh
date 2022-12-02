e69cb4eedebe270c41e9907fa2225fec7ac380f4
chatofking <chat_4432@hotmail.com>
2022-12-02 19:29:46 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install neofetch htop -y
-curl https://raw.githubusercontent.com/Cretezy/Swap/master/swap.sh -O swap
+curl https://raw.githubusercontent.com/Cretezy/Swap/master/swap.sh -o swap
 sh swap 4G
 sudo apt install docker.io docker-compose -y
 sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
